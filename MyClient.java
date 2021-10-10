ffa8dc7d130228145491f5a25b32150b5fc97b4b
harryBarnes1991 <92231975+harryBarnes1991@users.noreply.github.com>
2021-10-09 21:37:03 -0400

    Added Documentation and Inline Comments

--- MyClient.java
+++ MyClient.java
@@ -1,38 +1,68 @@
+/*
+	Group: Harrison Barnes, Long Lam, Adam Seidman
+	CMPE570/670 - Data & Communication Networks
+	Practical Exercise 1
+
+	This program provides functionality for the client in a client-server connection.
+	The client connects, provides a student name with three exam grades.
+	The client then disconnects upon receiving the exam average.
+ */
+
+//Import required java libraries
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
+/*
+	Class: 			MyClient
+	Functionality:	Connects to server. Provides a student name and three exam grades to average.
+					Receives averaged grades from server and prints to terminal.
+	Parent:			None
+	Children: 		None
+	Dependencies: 	None
+ */
 public class MyClient
 {
+	/*
+		Method:  		main
+		Arguments: 		String[] (not used)
+		Returns:		void
+		Preconditions: 	None
+		Functionality:	Connects to server. Provides a student name and three exam grades to average.
+						Receives averaged grades from server and prints to terminal.
+	 */
 	public static void main(String [] args)
 	{
-		try{
-			
+		try{	//Run until IOException
+			//Create a socket to port 16789 and establish IO streams and PrintWriter
 			Socket s = new Socket( "localhost", 16789);
 			InputStream in = s.getInputStream();
 			OutputStream out = s.getOutputStream();
-			
 			PrintWriter pout = new PrintWriter(out);
 
-			//Getting input from user
+			//Prompt and receive input from user
 			Scanner myScanner = new Scanner(System.in);
 			System.out.println("Please enter student name and the 3 exams scores (Separate the name and each exam scores with a comma)");
 			String userinput = myScanner.nextLine();
-			pout.println(userinput);		// Writes some String to server
-			pout.flush(); 					// forces the data through to server
-			
+
+			//Send string to server
+			pout.println(userinput);		//Writes some String to server
+			pout.flush(); 					//Forces the data through to server
+
+			//Establish receive buffer and wait for transmission from server
 			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+			//Print string from server transmission to terminal
 			System.out.println(userinput+" <=returned-as=> " + bin.readLine());
 
+			//Close connection and data streams
 			out.close();
 			pout.close();
 			s.close();
 		}
-		catch(IOException ioe)
-		{
-			System.out.println("IO error");
-			ioe.printStackTrace();
+		catch(IOException ioe) { //Catch and handle IOExceptions
+			System.out.println("IO error");	//Notify user of exception
+			ioe.printStackTrace();			//Print stack trace for debugging and error information
 		}
-	   
 	}
 }
--- MyServer.java
+++ MyServer.java
@@ -1,58 +1,132 @@
+/*
+	Group: Harrison Barnes, Long Lam, Adam Seidman
+	CMPE570/670 - Data & Communication Networks
+	Practical Exercise 1
+
+	This program provides functionality for the server in a client-server connection.
+	The client connects, provides a student name with three exam grades.
+	The server calculates the average grade, stores the student and the average in the server.
+	The server transmits the average grade back to the client.
+	The client is threaded with relevant synchronizations to handle multiple clients error-free.
+ */
+
+//Import required java libraries
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+	Class: 			MyServer
+	Functionality:	Receives student grade data from clients. Calculates average exam score. Exam averages are
+					stored internally and transmitted to the client. MyServer is able to handle multiple clients.
+	Parent:			None
+	Children: 		ThreadServer (inner Class)
+	Dependencies: 	None
+ */
 public class MyServer{
+	/*
+		Method:  		main
+		Arguments: 		String[] (not used)
+		Returns:		void
+		Preconditions: 	None
+		Functionality:	Static function. Creates a new server.
+	 */
 	public static void main(String [] args) {
 		new MyServer();
 	}
 
+	/*
+		Method:  		MyServer - Constructor
+		Arguments: 		None
+		Returns:		None
+		Preconditions: 	None
+		Functionality:	Establishes connections with one or more clients and creates a thread for each.
+	 */
 	public MyServer()
 	{
+		//Allocate memory for server socket for connections
 		ServerSocket ss = null;
 
-		List<List> student_list = new ArrayList<List>(); //2D list for storing student name and their
-		//average
+		//2D list for storing student name and their average
+		List<List> student_list = new ArrayList<List>();
 
-		try {
+		try {	//Run until IOException or BindException
+			//Initalize server socket to port 16789
 			ss = new ServerSocket(16789);
+
+			//allocate memory for client socket
 			Socket cs = null;
+
+			//Print server information to terminal
 			System.out.println("getLocalHost: "+InetAddress.getLocalHost() );
 			System.out.println("getByName:    "+InetAddress.getByName("localhost") + "\n" );
+
+			//Continually loop waiting for connections
+			//Terminates when program is terminated or an exception occurs
 			while(true){
-				cs = ss.accept(); 				// wait for connection
+				// wait for connection
+				cs = ss.accept(); //NOTE - Method blocks main thread until a connection is established
+
+				//Announce a connection of a client
 				System.out.println("A new client has been connected");
+
+				//Create a thread for the new client and begin thread
 				ThreadServer ths = new ThreadServer( cs, student_list);
 				ths.start();
 			} // end while
 		}
-		catch( BindException be ) {
-			System.out.println("Server already running on this computer, stopping.");
+		catch( BindException be ) {	//Catch and handle BindExceptions
+			System.out.println("Server already running on this computer, stopping."); //Notify user of exception
 		}
-		catch( IOException ioe ) {
-			System.out.println("IO Error");
-			ioe.printStackTrace();
+		catch( IOException ioe ) { //Catch and handle IOExceptions
+			System.out.println("IO Error");	//Notify user of exception
+			ioe.printStackTrace();			//Print stack trace for debugging and error information
 		}
 
 	} // end constructor
 
+	/*
+		Class: 			ThreadServer
+		Functionality:	Receives student grade data from clients. Calculates average exam score. Exam averages are
+						stored internally and transmitted to the client. MyServer is able to handle multiple clients.
+		Parent:			MyServer (inner Class of MyServer)
+		Children: 		None
+		Dependencies: 	None
+ 	 */
 	class ThreadServer extends Thread {  // member inner class
-		Socket cs;
-		List<List> student_list;
+		Socket cs;					//Client socket
+		List<List> student_list;	//List of students
 
+		/*
+			Method:  		ThreadServer - Constructor
+			Arguments: 		cs - client socket
+							student_list - list of students
+			Returns:		None
+			Preconditions: 	Socket cs is a valid connection
+			Functionality:	Creates a ThreadServer object
+		 */
 		public ThreadServer( Socket cs, List<List> student_list) {
 			this.cs = cs;
 			this.student_list = student_list;
 		}
 
+		/*
+			Method:  		run
+			Arguments: 		None
+			Returns:		void
+			Preconditions: 	Message is transmitted in format "NAME,GRADE1,GRADE2,GRADE3"
+			Functionality:	Handles client transmission and returns exam average
+		 */
 		public void run() {
+			//Initialize and allocate variables for operation
 			BufferedReader br;
 			PrintWriter opw;
-			String clientMsg;
-			int[] exams = new int[3];
+			String clientMsg;			//Message from client
+			int[] exams = new int[3];	//3 -> # of exam grades
 
-			try {
+			try {	//Run until IOException
+				//Initialize buffer reader and print writer for server use
 				br = new BufferedReader(
 						new InputStreamReader(
 								cs.getInputStream()));
@@ -60,52 +134,64 @@ public class MyServer{
 						new OutputStreamWriter(
 								cs.getOutputStream()));
 
-				clientMsg = br.readLine();					// from client
+				//Retrieve transmission string from client and echo to terminal
+				clientMsg = br.readLine();
 				System.out.println("Received from client: "+clientMsg);
+
+				//Tokenize string using ',' as a delimiter
 				String[] temp = clientMsg.split(",");
-				String student_name = temp[0];
-				for (int i = 0; i<3 ; i++){
+				String student_name = temp[0];	//Set student name to token zero
+				for (int i = 0; i<3 ; i++){	//Parse each token into a usable integer
 					exams[i] = Integer.parseInt(temp[i+1]);
 				}
+
+				//Calculate average of the student exam grades and trasmit average to client
 				System.out.print("Calculating the average-----");
-				double average = calculate_average(exams);
+				double average = calculate_average(exams);	//call synchronized function
 				System.out.println(average);
-				add_to_list(student_name, average);
-				opw.println(student_name+ ", your average is  " + average);	//to client
+				add_to_list(student_name, average);			//Add student name and average to server's list
+				opw.println(student_name+ ", your average is  " + average);	//Send average to client
 				opw.flush();
 			}
-			catch( IOException e ) {
-				System.out.println("Inside catch");
-				e.printStackTrace();
+			catch( IOException ioe ) { //Catch and handle IOExceptions
+				System.out.println("IO Error");	//Notify user of exception
+				ioe.printStackTrace();			//Print stack trace for debugging and error information
 			}
 		} // end while
 
-
+		/*
+			Method:  		calculate_average
+			Arguments: 		exams - array of exam grades stored as integers
+			Returns:		average exam grade
+			Preconditions: 	exams is an array with exactly 3 elements
+			Functionality:	Calculates the average grade based on the three exam grades
+		 */
 		public double calculate_average(int[] exams){
-
+			//Returns the average of the 3 exam grades
 			return (exams[0]+exams[1]+exams[2]) / 3.0;
 		}
 
+		/*
+			Method:  		add_to_list
+			Arguments: 		student_name - String containing the name of the student
+							average - average exam grade
+			Returns:		void
+			Preconditions: 	None
+			Functionality:	Stores the student and their average into the static list of student grades
+			Synchronized:	Only one client can edit at a time
+		 */
 		public synchronized void add_to_list (String student_name, double average){
-
-			synchronized (student_list) {
-
-				ArrayList<Serializable> temp_list = new ArrayList<>();
-				temp_list.add(student_name);
-				temp_list.add(average);
-				student_list.add(temp_list);
-				System.out.println("Updated student list:");
-				for (List row : student_list) {
+			synchronized (student_list) {	//Synchronize on student_list to prevent data corruption
+				ArrayList<Serializable> temp_list = new ArrayList<>();	//Create temporary list
+				temp_list.add(student_name);							//Add student name
+				temp_list.add(average);									//Add student average
+				student_list.add(temp_list);							//Update student list with new entry
+				System.out.println("Updated student list:");			//Print status message to terminal
+				for (List row : student_list) {	//Print entire student list to terminal
 					System.out.println(row.get(0) + "," + row.get(1));
 				}
 
 			}
 		}
-
-
-
-
-
-
 	} // end class ThreadServer
 } // end MyMultiThreadedServer
